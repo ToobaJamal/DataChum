19f9f2b3e1b3cba66e761f050b6db96de027c242
Tooba <jtooba70@gmail.com>
2023-08-09 14:31:12 +0500

    Add Link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import Logo from "../assets/logo.svg"
+import { Link } from "react-router-dom"
 export const Header = () => {
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center">
+        <Link to={"/"} className="flex items-center">
             <img src={Logo} className="h-8 mr-3" alt="DataChum Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DataChum</span>
-        </a>
+        </Link>
         <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
