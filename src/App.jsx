783067eef5565300d136cc77fd40ebff5677c498
Tooba <jtooba70@gmail.com>
2023-08-15 11:53:07 +0500

    Update imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import './App.css'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { HomePage } from './pages/Home/HomePage'
-import { Analysis } from './pages/Analysis'
+import { Analysis } from './pages/Analysis/Analysis'
 
 function App() {
-
   return (
     <>
     <Header />
