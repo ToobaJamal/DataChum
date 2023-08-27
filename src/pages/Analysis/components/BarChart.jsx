d7eaa0e95d88bbcb545a8165ad25719e709718cd
Tooba <jtooba70@gmail.com>
2023-08-27 14:25:10 +0500

    Add label and chart options

--- src/pages/Analysis/components/BarChart.jsx
+++ src/pages/Analysis/components/BarChart.jsx
@@ -17,14 +17,34 @@ const cleanData = dataComp[0].df.drop({ index: [lastIndex] });
     labels: compVals.map(vals => vals[xIndex]),
     datasets: [
       {
+        label: `${compCols[xIndex]} vs ${compCols[yIndex]}`,
         data: compVals.map(vals => vals[yIndex]),
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
       },
     ],
   };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: `${compCols[xIndex]}`,
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: `${compCols[yIndex]}`,
+        },
+      },
+    },
+  }
   return (
     <>
-        <Bar data={data}/>
+        <Bar data={data} options={options}/>
     </>
   )
 }
