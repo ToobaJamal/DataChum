79abda143065b5470ea806938c6641d74a18b6fa
Tooba <jtooba70@gmail.com>
2023-08-31 14:03:44 +0500

    Refactor sidebar styling

--- src/pages/Analysis/components/Sidebar.jsx
+++ src/pages/Analysis/components/Sidebar.jsx
@@ -34,7 +34,7 @@ export const Sidebar = ({setVisualize, xAxis, setXAxis, yAxis, setYAxis, dataCom
 
   return (
     <>
-        <div id="drawer-navigation" className="absolute right-0 h-full overflow-y-auto top-15 z-40 w-64 p-4 bg-white dark:bg-gray-800" aria-labelledby="drawer-navigation-label">
+        <div id="drawer-navigation" className="absolute right-0 h-4/5 overflow-y-auto z-40 w-64 p-4 bg-white dark:bg-gray-800" aria-labelledby="drawer-navigation-label">
             <button onClick={() => setVisualize(false)} type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                 <span className="sr-only">Close menu</span>
